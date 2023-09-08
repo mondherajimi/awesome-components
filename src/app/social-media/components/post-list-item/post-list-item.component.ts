359fc318210a71ca050d5ba4cefd73a885e33f10
ajimi mondher <ajimimondher@hotmail.com>
2023-09-08 07:28:37 +0100

    Add files via upload

---
+++ src/app/social-media/components/post-list-item/post-list-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { Post } from '../../../core/models/post.model';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+
+  @Input() post!: Post;
+  @Output() postCommented = new EventEmitter<{ comment: string, postId: number }>();
+  
+  tempUser = { firstName: 'Will', lastName: 'Alexander' }
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+
+  }
+  
+  onNewComment(comment : string) {
+	this.postCommented.emit({ comment, postId: this.post.id });
+  }
+
+}
